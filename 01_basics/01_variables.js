0eba125dc94e8ce06ccdc2b5c6739ec356779eb7
arslanrasheedacca <iori.arslan@gmail.com>
2025-12-05 13:05:38 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "iori.arslan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lahore"
+
+
+//accountId = 2   no allowed
+accountEmail = "hc@masdk.com"
+accountPassword = "1221212"
+accountCity = "rahimyar khan"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
